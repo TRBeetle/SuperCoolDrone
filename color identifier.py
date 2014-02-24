23ca44ee40245ad8e6f3c8f5d12fff2c27ce196e
magcayaz <magcayaz@gmu.com>
2014-02-24 14:18:27 -0500

    Color identifier
    Color identifier no works without having blue in the picture.

--- color identifier.py
+++ color identifier.py
@@ -19,37 +19,38 @@ width,height = c.get(3),c.get(4)
 print "frame width and height : ", width, height
 
 while(1):
-    _,f = c.read()
-    f = cv2.flip(f,1)
-    blur = cv2.medianBlur(f,5)
-    hsv = cv2.cvtColor(f,cv2.COLOR_BGR2HSV)
-    both = getthresholdedimg(hsv)
-    erode = cv2.erode(both,None,iterations = 3)
-    dilate = cv2.dilate(erode,None,iterations = 10)
+	_,f = c.read()
+	f = cv2.flip(f,1)
+	blur = cv2.medianBlur(f,5)
+	hsv = cv2.cvtColor(f,cv2.COLOR_BGR2HSV)
+	both = getthresholdedimg(hsv)
+	erode = cv2.erode(both,None,iterations = 3)
+	dilate = cv2.dilate(erode,None,iterations = 10)
 
-    contours,hierarchy = cv2.findContours(dilate,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-	print f[360][240]
+	contours,hierarchy = cv2.findContours(dilate,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+	lcenter = (int(360),int(240))
 	cv2.circle(f,lcenter,3,(0,255,0),2)
-    for cnt in contours:
+	print f[360][240]
+	for cnt in contours:
 		(x,y),radius = cv2.minEnclosingCircle(cnt)
-        #cx,cy = x+w/2, y+h/2
+		#cx,cy = x+w/2, y+h/2
 		lcenter = (int(360),int(240))
 		center = (int(x),int(y))
 		print f[360][240]
 		radius = int(radius)
 		cv2.circle(f,lcenter,3,(0,255,0),2)
 		#print "blue(x, y, radius) :", y,x, radius
-    #    if 20 < hsv.item(cy,cx,0) < 30:
-    #        cv2.rectangle(f,(x,y),(x+w,y+h),[0,255,255],2)
-    #        print "yellow :", y,x,w,h
-    #    elif 100 < hsv.item(cy,cx,0) < 120:
-    #        cv2.rectangle(f,(x,y),(x+w,y+h),[255,0,0],2)
-            #print "blue :", x,y,w,h
+		#    if 20 < hsv.item(cy,cx,0) < 30:
+		#        cv2.rectangle(f,(x,y),(x+w,y+h),[0,255,255],2)
+		#        print "yellow :", y,x,w,h
+		#    elif 100 < hsv.item(cy,cx,0) < 120:
+		#        cv2.rectangle(f,(x,y),(x+w,y+h),[255,0,0],2)
+			#print "blue :", x,y,w,h
 
-    cv2.imshow('img',f)
+		cv2.imshow('img',f)
 
-    if cv2.waitKey(25) == 27:
-        break
+	if cv2.waitKey(25) == 27:
+		break
 
 cv2.destroyAllWindows()
 c.release()
